8764685eb1ebe3788b5d1b3a3c4675de3b5fdc0c
purpleow1 <purpleowllight@gmail.com>
2017-02-02 23:14:03 +0300

    guessing-game/feature: implemented GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.left;
+    	this.right;
+    }
 
     setRange(min, max) {
-
+    	this.left = min;
+    	this.right = max;
     }
 
     guess() {
-
+    	return Math.ceil((this.left + this.right) / 2);
     }
 
     lower() {
-
+    	this.right = Math.ceil((this.left + this.right) / 2);
     }
 
     greater() {
-
+    	this.left = Math.ceil((this.left + this.right) / 2);
     }
 }
 
